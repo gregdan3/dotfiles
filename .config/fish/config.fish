04895701565915a9a4ad4a2dc3751b6f47ce3533
Gregory Danielson <gregory.danielson3@gmail.com>
2020-11-16 12:08:30 -0600

    Catch various typoes of vim and alias them

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,15 @@
-alias cp="cp -i"                #  confirm before overwriting something
+alias cp="cp -i"                # confirm before overwrite
 alias df='df -h'                # human-readable sizes
 alias du='du -h'                # ditto
 alias free='free -m'            # show sizes in MB
 alias rm='rm -iv'               # confirm before deletion
 alias ':q'=exit
 alias ':e'=vim
+alias v=vim
 alias vi=vim
+alias vm=vim
+alias ivm=vim
+alias vmi=vim
 
 bind \e\[P delete-char
 
