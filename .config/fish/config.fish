8936038c85f4800ade68e9403ccecbdc78a0ce86
Gregory Danielson <gregory.danielson3@gmail.com>
2020-12-01 11:25:17 -0600

    Replace task[l|p] abbreviations with aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,9 @@ alias vi=vim
 alias vm=vim
 alias ivm=vim
 alias vmi=vim
+alias task='~/.task/task-git.sh'
+alias taskp='~/.task/task-git.sh --push'
+alias taskl='~/.task/task-git.sh --pull'
 
 bind \e\[P delete-char
 
