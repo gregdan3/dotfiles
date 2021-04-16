2fcafec87c32ab05ed80a1cec025f1e636978244
Gregory Danielson <gregory.danielson3@gmail.com>
2021-04-16 11:20:01 -0500

    Organize config.fish, with new local PATH addition

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+bind \e\[P delete-char
+
 alias cp="cp -i"                # confirm before overwrite
 alias df='df -h'                # human-readable sizes
 alias du='du -h'                # ditto
@@ -11,9 +13,9 @@ alias vm=vim
 alias ivm=vim
 alias vmi=vim
 
-bind \e\[P delete-char
-
 # alias for git directory belonging to my dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 thefuck --alias oof | source
+
+set PATH $PATH /home/gregdan3/.local/bin
