9e476f4d847e0f9e630eb1f9ff066084bc66d22d
Gregory Danielson <gregory.danielson3@gmail.com>
2022-04-11 18:44:07 -0500

    Update TS config for nvim to avoid deprecated 'maintained' flag

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,14 +1,39 @@
-local userPlugins = require 'custom.plugins'
+local userPlugins = require "custom.plugins"
 
 local M = {}
 
-M.ui = {theme = "chadracula"}
+M.ui = { theme = "chadracula" }
 
 M.plugins = {
-    default_plugin_config_replace = {
-        nvim_treesitter = {ensure_installed = "maintained"}
-    },
-    options = {lspconfig = {setup_lspconf = "custom.plugins.lspconfig"}},
-    install = userPlugins
+   default_plugin_config_replace = {
+      nvim_treesitter = {
+         ensure_installed = {
+            "bash",
+            "c",
+            "cmake",
+            "comment",
+            "cpp",
+            "css",
+            "cuda",
+            "dockerfile",
+            "fish",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "make",
+            "python",
+            "regex",
+            "rust",
+            "toml",
+            "vim",
+            "yaml",
+         },
+         sync_install = false,
+         highlight = { enable = true, use_languagetree = true },
+      },
+   },
+   options = { lspconfig = { setup_lspconf = "custom.plugins.lspconfig" } },
+   install = userPlugins,
 }
 return M
