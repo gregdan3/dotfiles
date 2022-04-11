cb29e9668625fcac0b81d41dee2bb9b18dbbbf73
Gregory Danielson <gregory.danielson3@gmail.com>
2022-04-11 16:45:33 -0500

    Correct update.sh to point at pipx file and update

--- .config/archpaks/update.sh
+++ .config/archpaks/update.sh
@@ -4,5 +4,5 @@ echo "Running pacman -Qqne > ./driver"
               pacman -Qqne > ./driver
 echo "Running pacman -Qqme > ./external"
               pacman -Qqme > ./external
-echo "Running pipx list | grep - | sed 's/.* - //g' > ./pdm"
-              pipx list | grep - | sed 's/.* - //g' > ./pdm
+echo "Running pipx list | grep - | sed 's/.* - //g' > ./pipx"
+              pipx list | grep - | sed 's/.* - //g' > ./pipx
