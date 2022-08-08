e65d996fbe95a998cec1e0b95246181874b21d4f
Gregory Danielson <gregory.danielson3@gmail.com>
2022-08-08 13:46:44 -0500

    Update packages, with separate useful packages list `special`

--- .config/archpaks/update.sh
+++ .config/archpaks/update.sh
@@ -4,5 +4,6 @@ echo "Running pacman -Qqne > ./driver"
               pacman -Qqne > ./driver
 echo "Running pacman -Qqme > ./external"
               pacman -Qqme > ./external
-echo "Running pipx list | grep - | sed 's/.* - //g' > ./pipx"
-              pipx list | grep - | sed 's/.* - //g' > ./pipx
+
+# echo "Running pipx list | grep - | sed 's/.* - //g' > ./pipx"
+#               pipx list | grep - | sed 's/.* - //g' > ./pipx
