9b7fcf26c197f757b94aa39222cab16ccef1f60d
Gregory Danielson <gregory.danielson3@gmail.com>
2022-09-23 15:00:42 -0500

    Update lvim config with goodies

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -31,7 +31,9 @@ vim.opt.backspace = "indent,eol,start"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.encoding = "utf8"
 vim.opt.history = 10000
-vim.opt.mouse = "a"
+vim.opt.timeoutlen = 200
+-- vim.opt.mouse = ""
+-- vim.opt.mousemodel = ""
 vim.opt.shell = "/bin/fish"
 vim.opt.wildmode = "longest,full"
 vim.cmd([[filetype plugin indent on]])
@@ -47,6 +49,7 @@ vim.keymap.set({ "n", "i" }, "<Up>", "<NOP>", { silent = true })
 vim.keymap.set({ "n", "i" }, "<Down>", "<NOP>", { silent = true })
 vim.keymap.set({ "n", "i" }, "<Left>", "<NOP>", { silent = true })
 vim.keymap.set({ "n", "i" }, "<Right>", "<NOP>", { silent = true })
+vim.keymap.set({ "i" }, "jj", "<ESC>", { silent = true })
 
 -- lvim
 lvim.log.level = "warn"
@@ -55,13 +58,25 @@ lvim.colorscheme = "onedarker"
 lvim.leader = "space"
 
 lvim.builtin.alpha.active = false
-lvim.builtin.gitsigns.opts.current_line_blame = true
+lvim.builtin.nvimtree.active = false
+lvim.builtin.lir.active = false
+lvim.builtin.illuminate.active = false
+
+lvim.builtin.breadcrumbs.active = false
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 
+lvim.builtin.terminal.open_mapping = "<c-t>"
+lvim.builtin.gitsigns.opts.current_line_blame = false
+
 lvim.builtin.which_key.mappings.P = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings[";"] = nil -- needed if alpha is disabled
--- lvim.builtin.which_key.mappings.g.d -- TODO: diff against staged?
+lvim.builtin.which_key.mappings.g.D = { "<cmd>Gitsigns diffthis HEAD<cr>", "Git Diff" }
+lvim.builtin.which_key.mappings.g.d = { "<cmd>Gitsigns diffthis<cr>", "Git Diff" }
+
+lvim.builtin.which_key.mappings.T.m = { "<cmd>TSModuleInfo<CR>", "Module info" }
+lvim.builtin.which_key.mappings.T.u = { "<cmd>TSUpdate<CR>", "Update" }
+lvim.builtin.which_key.mappings.T.U = { "<cmd>TSUpdateSync<CR>", "Update+sync" }
 
 -- lvim.builtin.which_key.mappings.g.w = {
 -- 	"<cmd>lua lvim.builtin.gitsigns.opts.word_diff = not lvim.builtin.gitsigns.opts.word_diff<CR>",
@@ -100,18 +115,16 @@ formatters.setup({
 	{
 		command = "prettier",
 		extra_args = { "--print-width", "80" },
-		filetypes = { "typescript", "typescriptreact", "html", "css", "markdown", "yaml" },
+		filetypes = { "javascript", "js", "typescript", "typescriptreact", "html", "css", "markdown", "yaml" },
 	},
 	{ command = "rustfmt", filetypes = { "rust" } },
 	{ command = "shfmt", filetypes = { "sh", "bash" } },
 	{ command = "stylua", filetypes = { "lua" } },
+	-- { command = "sql-formatter", filetypes = { "sql" } },
 })
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-	{
-		command = "shellcheck",
-		extra_args = { "--severity", "warning" },
-	},
+	{ command = "shellcheck", filetypes = { "sh", "bash" } },
 	{ command = "checkmake", filetypes = { "Makefile" } },
 	{ command = "hadolint", filetypes = { "docker" } },
 })
@@ -125,9 +138,25 @@ lvim.plugins = {
 			require("lightspeed")
 		end,
 	},
+	{
+		"jayp0521/mason-null-ls.nvim",
+		after = {
+			"null-ls.nvim",
+			"mason.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				auto_update = true,
+				automatic_installation = true,
+			})
+		end,
+	},
 }
 
 vim.api.nvim_create_autocmd(
 	"BufReadPost",
 	{ pattern = "*", command = [[if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif]] }
 )
+
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
