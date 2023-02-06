c1c43494327add371af904d9c18a5fa88b9b921b
Gregory Danielson <gregory.danielson3@gmail.com>
2023-02-06 13:53:46 -0600

    Expand nvim config with remaining plugins shipped from LunarVim

--- .config/nvim/lua/plugins/main.lua
+++ .config/nvim/lua/plugins/main.lua
@@ -1,6 +1,7 @@
+local logic = require("mason-core.functional.logic")
 return {
-	-- { import = "lazyvim.plugins.extras.ui.mini-starter" },
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
+	{ import = "lazyvim.plugins.extras.ui.mini-starter" },
+	-- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	-- { "folke/which-key.nvim", opts = { mappings = {} } },
 	{
@@ -8,6 +9,7 @@ return {
 		auto_install = true,
 		opts = {
 			ensure_installed = {
+				"astro",
 				"bash",
 				"help",
 				"html",
@@ -20,6 +22,7 @@ return {
 				"query",
 				"regex",
 				"rust",
+				"svelte",
 				"tsx",
 				"typescript",
 				"vim",
@@ -35,8 +38,10 @@ return {
 			local nls = require("null-ls")
 			return {
 				sources = {
+					-- nls.builtins.diagnostics.pydocstyle,
+
 					nls.builtins.formatting.beautysh,
-					nls.builtins.formatting.prettier,
+					nls.builtins.formatting.prettier.with({ extra_filetypes = { "svelte", "astro", "mdx", "svx" } }),
 					nls.builtins.formatting.rubocop,
 					nls.builtins.formatting.rustfmt,
 					nls.builtins.formatting.shellharden,
@@ -53,6 +58,55 @@ return {
 					nls.builtins.formatting.trim_whitespace,
 				},
 			}
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		keys = function()
+			return {}
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-emoji",
+		},
+		---@param opts cmp.ConfigSchema
+		opts = function(_, opts)
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
+			local luasnip = require("luasnip")
+			local cmp = require("cmp")
+
+			opts.mapping = vim.tbl_extend("force", opts.mapping, {
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+					-- they way you will only jump inside the snippet region
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			})
 		end,
 	},
 
@@ -76,12 +130,6 @@ return {
 		opts = {
 			open_mapping = "<c-t>",
 			direction = "float",
-
-			execs = {
-				{ nil, "<M-1>", "Horizontal Terminal", "horizontal", 0.3 },
-				{ nil, "<M-2>", "Vertical Terminal", "vertical", 0.4 },
-				{ nil, "<M-3>", "Float Terminal", "float", nil },
-			},
 		},
 	},
 	{ "RRethy/vim-illuminate" }, -- highlight same symbol elsewhere
